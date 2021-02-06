3817b662e97435e562b8c91b9d0ce0fdcf76f179
jeriannh <jlhsiao@uwaterloo.ca>
2021-02-06 15:07:05 -0500

    Changed mobile spacing

--- src/components/layout.js
+++ src/components/layout.js
@@ -32,7 +32,7 @@ export default function Layout({ children, title }) {
       </Helmet>
       <body className="bg-white flex flex-col min-h-screen">
         {/*{announcementBanner}*/}
-        <div className="mx-8 mb-32 flex-grow md:mx-16 lg:mt-2 lg:mx-32 xl:mx-64 2xl:mx-96">
+        <div className="mx-6 mb-32 flex-grow md:mx-16 lg:mt-2 lg:mx-32 xl:mx-64 2xl:mx-96">
           <Header />
           <div className="my-8">{children}</div>
         </div>
